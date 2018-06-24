be1d9d443d280e9c0d56443604de2b184ed671b0
NicolasAbroad <40135543+NicolasAbroad@users.noreply.github.com>
2018-06-24 22:44:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ import zipfile
 import random
 import re
 
-res = requests.get('https://ncode.syosetu.com/n8611bv/')            # Change to input
-res.raise_for_status()
-toc = bs4.BeautifulSoup(res.content, "html.parser")
-rand = 'nicolas' + str(random.randint(100000000000,999999999999))   # Random string for id
-
 def series(soup):
     return soup.select('.novel_title')[0].getText()                 # Series name
 
@@ -34,23 +29,6 @@ def chapter_names_and_urls(soup):
         elif 'subtitle' in str(child.encode('utf-8')):
             url_dict[volume] += [(child.find('a').text, child.a['href'])]
     return url_dict
-
-def c2(soup):
-    index = soup.find('div', class_='index_box')
-    children = index.children
-    url_dict = {}
-    volume = 0
-    for child in children:
-        if 'chapter_title' in str(child.encode('utf-8')):
-            url_dict.setdefault(child.string, [])
-            volume = child.string
-        elif 'subtitle' in str(child.encode('utf-8')):
-            url_dict[volume] += [child.find('a').text]
-    return url_dict
-
-#for key in c2(toc):
-#    for i in range(len(c2(toc)[key])):
-#        print(c2(toc)[key][i])
 
 def create_folders(volume_name):                # Enters volume directory
     try:
@@ -76,15 +54,15 @@ def create_essential_files():
     os.chdir('.\\META-INF')
     container = open('container.xml', 'w')
     container.write('''<?xml version="1.0"?>
-        <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
-          <rootfiles>
-            <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
-          </rootfiles>
-        </container>''')
+<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+    <rootfiles>
+        <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
+    </rootfiles>
+</container>''')
     container.close()
     os.chdir('..\\')
 
-def toc_create_start(uid, book_name):          # Creates start of toc.ncx string
+def toc_create_start(uid, volume_name):          # Creates start of toc.ncx string
     toc_string = '''<?xml version="1.0" encoding="UTF-8"?>
     <ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
         <head>
@@ -94,11 +72,11 @@ def toc_create_start(uid, book_name):          # Creates start of toc.ncx string
                 <meta name="dtb:maxPageNumber" content="0"/>
         </head>
         <docTitle>
-        <text>''' + book_name + '''</text>
+        <text>''' + volume_name + '''</text>
         </docTitle>'''
     return toc_string
 
-def toc_add_nav(toc_string, chapter_name):      # Adds one nav point to toc.ncx string
+def toc_add_nav(toc_string, chapter_name, order):      # Adds one nav point to toc.ncx string
     toc_string += '\n' + '''<navMap>
         <navPoint id="''' + chapter_name + '''" playOrder ="''' + str(order) + '''">
             <navLabel>
@@ -110,15 +88,17 @@ def toc_add_nav(toc_string, chapter_name):      # Adds one nav point to toc.ncx 
 
 def toc_finish(toc_string):                     # Adds final lines to toc.ncx string and creates file
     toc_string += '\n</navMap>\n</ncx>'
+    os.chdir('.\\OEBPS')
     toc_file = open('toc.ncx', mode='w')
     toc_file.write(toc_string)
     toc_file.close()
+    os.chdir('..\\')
 
-def content_create_start(book_name, author_name, uid):         # Creates start of content.opf string
+def content_create_start(volume_name, author_name, uid):         # Creates start of content.opf string
     content_string = '''<?xml version="1.0" encoding="UTF-8"??>
 <package xmlns="http://www.idpf.org/2007/opf" unique-identifier="BookID" version="2.0" >
     <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
-        <dc:title>''' + book_name + '''</dc:title>
+        <dc:title>''' + volume_name + '''</dc:title>
         <dc:creator opf:role="aut">''' + author_name + '''</dc:creator>
         <dc:language>ja-JP </dc:language>
         <dc:rights>Public Domain</dc:rights>
@@ -135,36 +115,47 @@ def content_add_item_id(content_string, chapter_name):              # Adds one i
     content_string += '        <item id ="' + chapter_name + '" href="' + chapter_name + '.xhtml" media-type="application/xhtml+xml" />\n'
     return content_string
 
-def content_create_middle(content_string):                          # Finishes manifest section and starts spine section in content.opf string
-    content_string += '    </manifest>\n    <spine  toc="ncx">\n'
+def content_create_middle():                          # Finishes manifest section and starts spine section in content.opf string
+    content_string = '    </manifest>\n    <spine  toc="ncx">\n'
     return content_string
 
-def content_add_itemref(content_string):                            # Adds one itemref to content.opf string
-    content_string += '        <itemref idref ="titlepage" />\n'
+def content_add_itemref(content_string, chapter_name):                            # Adds one itemref to content.opf string
+    content_string += '        <itemref idref ="' + chapter_name + '" />\n'
     return content_string
 
 def content_finish(content_string):                                 # Adds final lines to content.opf string and creates file
     content_string += '    </spine>\n</package>'
+    os.chdir('.\\OEBPS')
     content_file = open('content.opf', mode='w')
     content_file.write(content_string)
     content_file.close()
-
-ch_name = ['CHAPTER 1', 'CHAPTER 2', 'CHAPTER 3']
-ch_url = ['https://ncode.syosetu.com/n8611bv/1', 'https://ncode.syosetu.com/n8611bv/2', 'https://ncode.syosetu.com/n8611bv/3']
+    os.chdir('..\\')
 
 def chapter_get_string(url):                                        # Change argument to extracted chapter url list
     url = 'https://ncode.syosetu.com/n8611bv/1'                     # Change to input
     r = requests.get(url)
     r.raise_for_status()
     page = bs4.BeautifulSoup(r.content, "html.parser")
-    return str(page.find('div', attrs={'id':'novel_color'}))
+    chapter_string = str(page.find('div', attrs={'id':'novel_color'}))
+    chapter_string = chapter_string.encode('utf-8')
+    return chapter_string
 
 def chapter_create_file(chapter_name, chapter_string):
-    os.chdir('.\\OEBPS')                                                    # Change directory before using function
-    chapter_file = open(chapter_name + '.xhtml', 'w')
+    os.chdir('.\\OEBPS')
+    chapter_file = open(chapter_name + '.xhtml', 'wb')
+    chapter_head = '''<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title></title>
+</head>
+<body>''' + '\n'
+    chapter_head = chapter_head.encode('utf-8')
+    chapter_file.write(chapter_head)
     chapter_file.write(chapter_string)
+    chapter_file.write('</body>\n</html>'.encode('utf-8'))
     chapter_file.close()
-    os.chdir('..\\')                                                        # Change directory after using function
+    os.chdir('..\\')
 
 def zip_create(volume_name):
     volume_zip = zipfile.ZipFile(volume_name + '.zip', 'w')                 # TODO: Add number in front of file name
@@ -178,8 +169,53 @@ def zip_close(volume_name):
     volume_zip.close()
 
 
+input_url = 'https://ncode.syosetu.com/n8611bv/'                    # Change to user input
+res = requests.get(input_url)
+res.raise_for_status()
+toc = bs4.BeautifulSoup(res.content, "html.parser")
+rand = 'nicolas' + str(random.randint(100000000000,999999999999))   # Random string for id
+
+series_name = series(toc)
+author_name = author(toc)
+volume_names = list(chapter_names_and_urls(toc).keys())
+chapter = chapter_names_and_urls(toc)
+
+for i, volume in enumerate(volume_names):
+    volume_formatted_name = '{0:02d}'.format(i+1) + ' - ' + volume
+    create_folders(volume_formatted_name)
+    create_essential_files()
+
+    toc_string = toc_create_start(rand,volume_formatted_name)
+
+    content_string_first_half = content_create_start(volume_formatted_name, author_name, rand)
+    content_string_second_half = content_create_middle()
+
+    volume_chapters = chapter_names_and_urls(toc)[volume]
+    for tuple in range(len(volume_chapters)):
+        chapter_url_end_unformatted = re.compile(r'\d+/$').findall(volume_chapters[tuple][1])
+        chapter_url_end_formatted = chapter_url_end_unformatted[0][:-1]
+        chapter_url = input_url + chapter_url_end_formatted
+        chapter_string = chapter_get_string(chapter_url)
+        chapter_create_file(volume_chapters[tuple][0], chapter_string)
+        
+        toc_string = toc_add_nav(toc_string, volume_chapters[tuple][0], tuple)
+        
+        content_string_first_half = content_add_item_id(content_string_first_half, volume_chapters[tuple][0])
+        content_string_second_half = content_add_itemref(content_string_second_half, volume_chapters[tuple][0])
+        
+    toc_finish(toc_string)
+    content_string = content_string_first_half + content_string_second_half
+    content_finish(content_string)
+    os.chdir('..\\')
+    print('Completed: ' + volume_formatted_name)
 
 
+'''    REFERENCE LOOP
+for volume in volume_names:
+    print(volume + ':')
+    volume_chapters = chapter_names_and_urls(toc)[volume]
+    for tuple in range(len(volume_chapters)):
+        print(volume_chapters[tuple][0] + ': ' + volume_chapters[tuple][1])'''
 
 # Get author name
 # Get series name
