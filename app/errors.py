f743b0ac99815433fc44962ed78a6f38cd93beba
NicolasAbroad <nicolas.trufanow@gmail.com>
2018-10-09 23:49:12 +0900

    Heroku deployement update

--- app/errors.py
+++ app/errors.py
@@ -9,13 +9,18 @@ logger = logging.getLogger(__name__)
 
 @app.errorhandler(404)
 def not_found_error(error):
-    logger.info('%s - error 404' % current_user.username)
+    if current_user.is_authenticated:
+        logger.info('%s - error 404' % current_user.username)
+    else:
+        logger.info('Anonymous - error 404')
     return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    logger.info('%s - error 500' % current_user.username)
+    if current_user.is_authenticated:
+        logger.info('%s - error 500' % current_user.username)
+    else:
+        logger.info('Anonymous - error 500')
     db.session.rollback()
     return render_template('500.html'), 500
-
--- app/models.py
+++ app/models.py
@@ -4,12 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
-class User(UserMixin, db.Model):
+class users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    requests = db.relationship('RequestHistory', backref='author', lazy='dynamic')
+    requests = db.relationship('requesthistory', backref='author', lazy='dynamic')
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -22,14 +22,14 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
 
-class RequestHistory(db.Model):
+class requesthistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     series_name = db.Column(db.String(140), index=True)
     volume_name = db.Column(db.String(140), index=True)
     volume_number = db.Column(db.String(4))
     url = db.Column(db.String(1800))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
         return '<Request {}>'.format(self.body)
@@ -37,5 +37,4 @@ class RequestHistory(db.Model):
 
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id))
-
+    return users.query.get(int(id))
