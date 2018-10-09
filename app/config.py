f957e1cf84bcbc044d85b0ec8156c4ebe7ae5172
NicolasAbroad <nicolas.trufanow@gmail.com>
2018-10-09 11:33:55 +0900

    Heroku bug fix

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.urandom(20)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') + '?check_same_thread=False' \
-                              or 'sqlite:///' + os.path.join(basedir, 'app.db' + '?check_same_thread=False')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
+                              or 'sqlite:///' + os.path.join(basedir, 'wnscraper.db')
 #    DATABASE_URL=mysql+pymysql://wnscraper:<db-password>@localhost:3306/wnscraper
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LANGUAGES = ['en', 'ja']
